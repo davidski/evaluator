7177c28bf80b561ec2cafa7246ee1807707f163f
David F. Severski <davidski@deadheaven.com>
2017-01-27 09:30:59 -0800

    Finalize draft of risk dashboard. All components complete.

--- bin/common_graphs.R
+++ bin/common_graphs.R
@@ -1,17 +1,20 @@
 # ---- domain_heatmap ---
 
-dat <- domain_impact %>% mutate(full_label = paste0(domain_id, "\n", "$", 
-                                                    round(var / 10^6), "M"))
-gg <- ggplot(dat, aes(x = domain, y = 1, fill = var)) 
-gg <- gg + geom_tile(color="white")
-gg <- gg + geom_text(aes(x = domain, y = 1, label = full_label), 
+dat <- domain_impact %>% 
+  arrange(domain_id) %>% 
+  mutate(full_label = paste0(domain_id, "\n", "$", round(var / 10^6), "M"))
+dat$aux <- seq(1, 2*nrow(dat), by = 2)
+gg <- ggplot(dat, aes(x = aux, y = 1)) 
+gg <- gg + geom_tile(stat = "identity", color="white", aes(fill = var), 
+                     width = 1)
+gg <- gg + geom_text(aes(x = aux, y = 1, label = full_label), 
                      color = "white", size = 3)
 gg <- gg + coord_equal()
-gg <- gg + scale_fill_viridis(guide = guide_legend(title=NULL), 
-                              labels = dollar_millions)
-gg <- gg + labs(x = NULL, y=NULL,
+gg <- gg + scale_fill_viridis(guide = FALSE)
+gg <- gg + labs(x = NULL, y = NULL,
                 title = "Value at Risk by Domain",
                 caption = "Source: Evaluator toolkit") 
 gg <- gg + theme_evaluator() 
-gg <- gg + theme(axis.text = element_blank(), panel.grid = element_blank())
+gg <- gg + theme(axis.text = element_blank())
+gg <- gg + theme(panel.grid = element_blank())
 gg
