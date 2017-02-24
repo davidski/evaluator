4d52294c53a1a9b6fe57cbbff610902381ec5c2e
David F. Severski <davidski@deadheaven.com>
2017-02-24 14:32:18 -0800

    Clean up of load_data function

--- R/load_data.R
+++ R/load_data.R
@@ -8,31 +8,31 @@
 #' @return List of data objects
 #' @export
 load_data <- function(input_directory, results_directory) {
-  # full results
-  simulation_results <- NULL
+  # load simulation results and default summary objects
+  simulation_results <- NULL # detailed sumulation results
   load(file.path(results_directory, "simulation_results.rda"))
-  # scenario level summary
-  sceanrio_summary <- NULL
+  scenario_summary <- NULL   # scenario level summary
   load(file.path(results_directory, "scenario_summary.rda"))
-  # domain level summary
-  domain_summary <- NULL
+  domain_summary <- NULL     # domain level summary
   load(file.path(results_directory, "domain_summary.rda"))
 
-  # details on our scenarios
+  # all input files
   domains <- readr::read_csv(file.path(input_directory, "domains.csv"))  # domain catalog
   mappings <- readr::read_csv(file.path(input_directory, "qualitative_mappings.csv"))  # qualitative translations
   capabilities <- readr::read_csv(file.path(input_directory, "capabilities.csv"))  # i.e. objectives & controls
   risk_tolerances <- readr::read_csv(file.path(input_directory, "risk_tolerances.csv"))  # i.e. risk tolerances
-  scenarios <- readr::read_csv(file.path(input_directory, "qualitative_scenarios.csv")) %>%
+  qualitative_scenarios <- readr::read_csv(file.path(input_directory, "qualitative_scenarios.csv")) %>%
     mutate_("tef" = ~ tolower(tef), "lm" = ~ tolower(lm), "tc" = ~ tolower(tc))
 
-  # Precalculate the standard order of scenarios (domain, then ID of the scenario)
+  # precalculate the standard order of scenarios (domain, then ID of the scenario)
   scenario_order <- group_by_(simulation_results, "domain_id", "scenario_id") %>%
     summarise()
-  # store the `scenario_id` of outliers
-  scenario_outliers <- scenario_summary[which(scenario_summary$outlier == TRUE), "scenario_id"] %>% unlist %>% unname
-  # a text vector of numbers to english words
-  numbers <- c("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+  # build a vector of scenario outliers
+  scenario_outliers <- scenario_summary[which(scenario_summary$outlier == TRUE),
+                                        "scenario_id"] %>% unlist %>% unname
+
+  # build a named vector of risk tolerance threshold
   risk_tolerance <- risk_tolerances$amount
   names(risk_tolerance) <- risk_tolerances$level %>% tolower
 
@@ -42,10 +42,15 @@ load_data <- function(input_directory, results_directory) {
                                                            ifelse(ale_var >= risk_tolerance["medium"],
                                                                   "Medium", "Low"))) %>%
     mutate_(annual_tolerance = ~factor(annual_tolerance, levels = c("High", "Medium", "Low"), ordered = TRUE))
-  scenario_outliers <- scenario_summary[which(scenario_summary$outlier == TRUE), "scenario_id"] %>% unlist %>% unname
-  list(simulation_results = simulation_results, scenario_summary = scenario_summary,
-       domain_summary = domain_summary, domains = domains, mappings = mappings,
-       capabilities = capabilities, risk_tolerances = risk_tolerances,
+
+  list(simulation_results = simulation_results,
+       scenario_summary = scenario_summary,
+       domain_summary = domain_summary,
+       domains = domains,
+       mappings = mappings,
+       capabilities = capabilities,
+       risk_tolerances = risk_tolerances,
        risk_tolerance = risk_tolerance,
-       scenario_outliers = scenario_outliers, scenarios = scenarios)
+       scenario_outliers = scenario_outliers,
+       scenarios = qualitative_scenarios)
 }
