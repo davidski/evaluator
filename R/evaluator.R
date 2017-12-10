5a052d73865780540cce9212f6580e6e3bbedfde
David F. Severski <davidski@deadheaven.com>
2017-12-10 15:10:27 -0800

    Update main help page with correct links to READMEs, FAIR Institute, and Evaluator GitHub.

--- R/evaluator.R
+++ R/evaluator.R
@@ -3,14 +3,19 @@
 #' Information Security Risk Simulation Toolkit
 #'
 #' See the README on
-#' \href{https://cran.r-project.org/package=googlesheets/README.html}{CRAN}
-#' or \href{https://github.com/davidski/evaluator}{GitHub}
+#' [CRAN](https://cran.r-project.org/package=evaluator/README.html)
+#' or [GitHub](https://github.com/davidski/evaluator)
 #'
+#' @seealso
+#' Useful links:
+#' * [FAIR Institute](http://www.fairinstitute.org/)
+#' * Report bugs at [GitHub](https://github.com/davidski/evaluator/issues)
 #' @docType package
 #' @name evaluator
 #' @importFrom dplyr %>%
+#' @importFrom utils globalVariables
 NULL
 
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
 ## technique from Jenny Bryan's googlesheet package
-if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
+if (getRversion() >= "2.15.1")  utils::globalVariables(c("."))
