1e9b3ed4626419e98643ca64013de68e1410e016
David F. Severski <davidski@deadheaven.com>
2017-12-10 20:09:23 -0800

    Update links

--- R/evaluator.R
+++ R/evaluator.R
@@ -3,19 +3,14 @@
 #' Information Security Risk Simulation Toolkit
 #'
 #' See the README on
-#' [CRAN](https://cran.r-project.org/package=evaluator/README.html)
-#' or [GitHub](https://github.com/davidski/evaluator)
+#' \href{https://cran.r-project.org/package=googlesheets/README.html}{CRAN}
+#' or \href{https://github.com/davidski/evaluator}{GitHub}
 #'
-#' @seealso
-#' Useful links:
-#' * [FAIR Institute](http://www.fairinstitute.org/)
-#' * Report bugs at [GitHub](https://github.com/davidski/evaluator/issues)
 #' @docType package
 #' @name evaluator
 #' @importFrom dplyr %>%
-#' @importFrom utils globalVariables
 NULL
 
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
 ## technique from Jenny Bryan's googlesheet package
-if (getRversion() >= "2.15.1")  utils::globalVariables(c("."))
+utils::globalVariables(c("."))
