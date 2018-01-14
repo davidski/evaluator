5957cff5939aca8621e6720b9a6393fd304e6cdd
David F. Severski <davidski@deadheaven.com>
2018-01-14 12:06:51 -0800

    Complete report unit tests, adding better NSE for the report template.

--- tests/testthat/test-reports.R
+++ tests/testthat/test-reports.R
@@ -1,31 +1,43 @@
 context("Reports")
+tmpdir <- tempdir()
+tmpdata <- file.path(tmpdir, "data")
+tmpinputs <- file.path(tmpdir, "inputs")
+dir.create(tmpdata)
+dir.create(tmpinputs)
+
+data("simulation_results", package = "evaluator", envir = environment())
+save(simulation_results, file = file.path(tmpdata, "simulation_results.rda"))
+data("scenario_summary", package = "evaluator", envir = environment())
+save(scenario_summary, file = file.path(tmpdata, "scenario_summary.rda"))
+data("domain_summary", package = "evaluator", envir = environment())
+save(domain_summary, file = file.path(tmpdata, "domain_summary.rda"))
+
+res <- c("domains.csv", "qualitative_mappings.csv", "risk_tolerances.csv") %>%
+  purrr::map(~ file.copy(system.file("extdata", .x, package = "evaluator"),
+                         tmpinputs))
+data("capabilities", envir = environment())
+readr::write_csv(capabilities, file.path(tmpinputs, "capabilities.csv"))
+data("qualitative_scenarios", envir = environment())
+readr::write_csv(qualitative_scenarios, file.path(tmpinputs, "qualitative_scenarios.csv"))
+
+file <- tempfile(fileext = ".html")
+
 test_that("Analyze report renders", {
 
-  skip_on_cran()
-
-  tmpdir <- tempdir()
-  tmpdata <- file.path(tmpdir, "data")
-  tmpinputs <- file.path(tmpdir, "inputs")
-  dir.create(tmpdata)
-  dir.create(tmpinputs)
-
-  data("simulation_results", package = "evaluator", envir = environment())
-  save(simulation_results, file = file.path(tmpdata, "simulation_results.rda"))
-  data("scenario_summary", package = "evaluator", envir = environment())
-  save(scenario_summary, file = file.path(tmpdata, "scenario_summary.rda"))
-  data("domain_summary", package = "evaluator", envir = environment())
-  save(domain_summary, file = file.path(tmpdata, "domain_summary.rda"))
-
-  res <- c("domains.csv", "qualitative_mappings.csv", "risk_tolerances.csv") %>%
-    purrr::map(~ file.copy(system.file("extdata", .x, package = "evaluator"),
-                           tmpinputs))
-  data("capabilities", envir = environment())
-  readr::write_csv(domains, file.path(tmpinputs, "capabilities.csv"))
-  data("qualitative_scenarios", envir = environment())
-  readr::write_csv(qualitative_scenarios, file.path(tmpinputs, "qualitative_scenarios.csv"))
-
-  file <- tempfile(fileext = ".html")
-  # expect_success(generate_report(input_directory = tmpinputs,
-  #                                results_directory = tmpdata,
-  #                                output_file = file))
+  result <- evaluate_promise(generate_report(input_directory = tmpinputs,
+                                             results_directory = tmpdata,
+                                             output_file = file, quiet = TRUE))
+  expect_equivalent(normalizePath(result$result), normalizePath(file))
 })
+
+test_that("Risk Dashboard renders", {
+  result <- evaluate_promise(risk_dashboard(input_directory = tmpinputs,
+                                            results_directory = tmpdata,
+                                            output_file = file,
+                                            quiet = FALSE))
+  expect_equivalent(normalizePath(result$result), normalizePath(file))
+  # there should be no warnings
+  expect_condition(result$warnings, regexp = NA)
+})
+
+unlink(c(tmpdata, tmpinputs), recursive = TRUE)
