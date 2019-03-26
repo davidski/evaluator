eda575e4b7811be9bf921d73194b1ac9a25e7d90
David F. Severski <davidski@deadheaven.com>
2019-03-25 19:49:10 -0700

    Auxillary scrpt to regenerate demo reports

---
+++ scripts/generate_sample_reports.R
@@ -0,0 +1,39 @@
+devtools::load_all()
+
+tmpdir <- tempdir()
+tmpdata <- file.path(tmpdir, "data")
+dir.create(tmpdata)
+tmpinputs <- file.path(tmpdir, "inputs")
+dir.create(tmpinputs, showWarnings = FALSE)
+
+output_dir <- here::here("reports")
+dir.create(output_dir, showWarnings = FALSE)
+
+data("simulation_results", package = "evaluator", envir = environment())
+saveRDS(simulation_results, file = file.path(tmpdata, "simulation_results.rds"))
+data("scenario_summary", package = "evaluator", envir = environment())
+saveRDS(scenario_summary, file = file.path(tmpdata, "scenario_summary.rds"))
+data("domain_summary", package = "evaluator", envir = environment())
+saveRDS(domain_summary, file = file.path(tmpdata, "domain_summary.rds"))
+
+res <- c("domains.csv", "qualitative_mappings.csv", "risk_tolerances.csv") %>%
+  purrr::map(~ file.copy(system.file("extdata", .x, package = "evaluator"),
+                         tmpinputs))
+data("qualitative_scenarios", envir = environment())
+readr::write_csv(qualitative_scenarios, file.path(tmpinputs, "qualitative_scenarios.csv"))
+data("quantitative_scenarios", envir = environment())
+saveRDS(quantitative_scenarios, file.path(tmpinputs, "quantitative_scenarios.rds"))
+
+file <- file.path(output_dir, "evaluator_risk_analysis.html")
+
+generate_report(input_directory = tmpinputs,
+                results_directory = tmpdata,
+                output_file = file)
+
+file <- file.path(output_dir, "evaluator_risk_dashboard.html")
+
+risk_dashboard(input_directory = tmpinputs,
+               results_directory = tmpdata,
+               output_file = file)
+
+unlink(c(tmpdata, tmpinputs), recursive = TRUE)
