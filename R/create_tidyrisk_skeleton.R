1c3665695ba628fa55c044d662168bae2dd6913a
David F. Severski <davidski@deadheaven.com>
2019-04-10 08:24:52 -0700

    Add in creates single scenario mini-analysis

--- R/create_tidyrisk_skeleton.R
+++ R/create_tidyrisk_skeleton.R
@@ -8,11 +8,14 @@
 #' @importFrom rstudioapi insertText
 create_tidyrisk_scenario_skeleton <- function() {
   rstudioapi::insertText(text =
-    "tidyrisk_scenario(
+    "my_scen <- tidyrisk_scenario(
         tef_params = list(min = 1, mode = 10, max = 100, shape = 3, func = \"mc2d::rpert\"),
         tc_params = list(min = .20, mode = .30, max = .70, shape = 2, func = \"mc2d::rpert\"),
         diff_params = list(list(min = .25, mode = .50, max = .60, shape = 3, func = \"mc2d::rpert\")),
-        lm_params = list(min = 100, mode = 1000, max = 10000, shape = 3, func = \"mc2d::rpert\"))"
+        lm_params = list(min = 100, mode = 1000, max = 10000, shape = 3, func = \"mc2d::rpert\"))
+
+my_results <- run_simulation(my_scen, iterations = 1000)
+"
     )
 }
 
